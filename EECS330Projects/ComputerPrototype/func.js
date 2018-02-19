de098645950a080b42c3e8cdd56456807d2a16e5
MSNimo <mikebenim@gmail.com>
2018-02-18 23:03:54 -0600

    links fix

--- EECS330Projects/ComputerPrototype/func.js
+++ EECS330Projects/ComputerPrototype/func.js
@@ -1,19 +1,19 @@
 function learn_more() {
-     location.href = "/EECS330Projects/ComputerPrototype/learnmore.html";
+     location.href = "../../EECS330Projects/ComputerPrototype/learnmore.html";
 } 
 
 function duckworth_go() {
-    location.href = "/EECS330Projects/ComputerPrototype/duckworth.html";
+    location.href = "../../EECS330Projects/ComputerPrototype/duckworth.html";
 }
 
 function kirk_go() {
-    location.href = "/EECS330Projects/ComputerPrototype/learnmore.html";
+    location.href = "../../EECS330Projects/ComputerPrototype/learnmore.html";
 }
 
 function schakowsky_go() {
-    location.href = "/EECS330Projects/ComputerPrototype/learnmore.html";
+    location.href = "../../EECS330Projects/ComputerPrototype/learnmore.html";
 }
 
 function lasonde_go() {
-    location.href = "../learnmore.html";
+    location.href = "../../EECS330Projects/ComputerPrototype/learnmore.html";
 }
