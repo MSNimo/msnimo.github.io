f2c9951ed8fb7bddd4fb2d242fd6f964258fa643
MSNimo <mikebenim@gmail.com>
2020-05-10 17:48:31 -0500

    Implemented Filtered Masonry gallery for projects

--- scripts/masonrycode.js
+++ scripts/masonrycode.js
@@ -1,12 +1,34 @@
-// external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
-// init Masonry
 var $grid = $('.grid').masonry({
   itemSelector: '.grid-item',
   percentPosition: true,
-  columnWidth: '.grid-sizer'
+  columnWidth: '.grid-sizer',
+  isAnimated: true,
 });
-// layout Masonry after each image loads
+
 $grid.imagesLoaded().progress( function() {
   $grid.masonry();
 });  
+
+$(".filters .btn").click(function(e) {
+  e.preventDefault();
+
+  var filter = $(this).attr("data-filter");
+
+  $grid.masonryFilter({
+      filter: function () {
+          if (!filter) return true;
+          return $(this).attr("data-filter") == filter;
+      }
+  });
+});
+
+var btnContainer = document.getElementById("btn-container");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
